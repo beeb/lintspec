8b6fd10e1842bd39075bc81ddf5812f4f4e76b55
beeb <703631+beeb@users.noreply.github.com>
2025-02-19 13:34:26 +0100

    docs: lint module

--- src/lint.rs
+++ src/lint.rs
@@ -16,25 +16,46 @@ use crate::{
     utils::detect_solidity_version,
 };
 
+/// Diagnostics for a single Solidity file
 #[derive(Debug, Clone, Serialize, thiserror::Error)]
 #[error("Error")]
 pub struct FileDiagnostics {
+    /// Path to the file
     pub path: PathBuf,
+
+    /// Contents of the file (can be an empty string if pretty output is not activated)
     #[serde(skip_serializing)]
     pub contents: String,
+
+    /// Diagnostics, grouped by source item (function, struct, etc.)
     pub items: Vec<ItemDiagnostics>,
 }
 
+/// Diagnostics for a single source item (function, struct, etc.)
 #[derive(Debug, Clone, Serialize)]
 pub struct ItemDiagnostics {
+    /// The parent contract, interface or library's name
     pub parent: Option<Parent>,
+
+    /// The type of this source item (function, struct, etc.)
     pub item_type: ItemType,
+
+    /// The name of the item
     pub name: String,
+
+    /// The span of the item (for function-like items, only the declaration without the body)
     pub span: TextRange,
+
+    /// The diagnostics related to this item
     pub diags: Vec<Diagnostic>,
 }
 
 impl ItemDiagnostics {
+    /// Print the diagnostics for a single source item in a compact format
+    ///
+    /// The writer `f` can be stderr or a file handle, for example. The path to the file containing this source item
+    /// should be provided, as well as the current working directory where the command was launched from. This last
+    /// information is used to compute the relative path when possible.
     pub fn print_compact(
         &self,
         f: &mut impl io::Write,
@@ -64,6 +85,7 @@ impl ItemDiagnostics {
     }
 }
 
+/// A type of source item (function, struct, etc.)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Display)]
 #[serde(rename_all = "lowercase")]
 pub enum ItemType {
@@ -87,12 +109,24 @@ pub enum ItemType {
     Variable,
 }
 
+/// A single diagnostic related to NatSpec.
 #[derive(Debug, Clone, Serialize)]
 pub struct Diagnostic {
+    /// The span (text range) related to the diagnostic
+    ///
+    /// If related to the item's NatSpec as a whole, this is the same as the item's span.
+    /// For a missing param or return NatSpec, this is the span of the param or return item.
     pub span: TextRange,
     pub message: String,
 }
 
+/// Lint a file by identifying NatSpec problems.
+///
+/// This is the main business logic entrypoint related to using this library. The path to the Solidity file should be
+/// provided, and a compatible Solidity version will be inferred from the first version pragma statement (if any) to
+/// inform the parsing. [`ValidationOptions`] can be provided to control whether some of the lints get reported.
+/// The `drop_contents` parameter controls if the returned [`FileDiagnostics`] contains the original source code
+/// (false) or an empty string (true).
 pub fn lint(
     path: impl AsRef<Path>,
     options: &ValidationOptions,
