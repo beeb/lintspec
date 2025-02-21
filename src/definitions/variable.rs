5ea81463fe3ce9f948349638eb7fa910c1a2fc00
beeb <703631+beeb@users.noreply.github.com>
2025-02-21 15:49:04 +0100

    fix(variable): allow no natspec if we don't enforce inheritdoc

--- src/definitions/variable.rs
+++ src/definitions/variable.rs
@@ -24,6 +24,9 @@ pub struct VariableDeclaration {
 }
 
 impl VariableDeclaration {
+    /// Check whether this variable requires inheritdoc when we enforce it
+    ///
+    /// Public state variables must have inheritdoc.
     fn requires_inheritdoc(&self) -> bool {
         let parent_is_contract = matches!(self.parent, Some(Parent::Contract(_)));
         let public = self.attributes.visibility == Visibility::Public;
@@ -84,8 +87,12 @@ impl Validate for VariableDeclaration {
             span: self.span(),
             diags: vec![],
         };
-        // raise error if no NatSpec is available
+        // raise error if no NatSpec is available (unless we don't enforce inheritdoc)
         let Some(natspec) = &self.natspec else {
+            if !options.inheritdoc || !self.requires_inheritdoc() {
+                return out;
+            }
+            // we require natspec for inheritdoc
             out.diags.push(Diagnostic {
                 span: self.span(),
                 message: "missing NatSpec".to_string(),
@@ -104,18 +111,109 @@ impl Validate for VariableDeclaration {
                 span: self.span(),
                 message: "@inheritdoc is missing".to_string(),
             });
-            return out;
-        }
-        if !natspec
-            .items
-            .iter()
-            .any(|n| matches!(n.kind, NatSpecKind::Notice | NatSpecKind::Dev))
-        {
-            out.diags.push(Diagnostic {
-                span: self.span(),
-                message: "missing @notice or @dev".to_string(),
-            });
         }
         out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use semver::Version;
+    use slang_solidity::{cst::NonterminalKind, parser::Parser};
+
+    use super::*;
+
+    static OPTIONS: ValidationOptions = ValidationOptions {
+        inheritdoc: true,
+        constructor: false,
+        struct_params: false,
+        enum_params: false,
+    };
+
+    fn parse_file(contents: &str) -> VariableDeclaration {
+        let parser = Parser::create(Version::new(0, 8, 0)).unwrap();
+        let output = parser.parse(NonterminalKind::SourceUnit, contents);
+        assert!(output.is_valid(), "{:?}", output.errors());
+        let cursor = output.create_tree_cursor();
+        let m = cursor
+            .query(vec![VariableDeclaration::query()])
+            .next()
+            .unwrap();
+        let def = VariableDeclaration::extract(m).unwrap();
+        def.as_variable().unwrap()
+    }
+
+    #[test]
+    fn test_variable() {
+        let contents = "contract Test is ITest {
+            /// @inheritdoc ITest
+            uint256 public a;
+        }";
+        let res = parse_file(contents).validate(&OPTIONS);
+        assert!(res.diags.is_empty(), "{:#?}", res.diags);
+    }
+
+    #[test]
+    fn test_variable_no_natspec() {
+        let contents = "contract Test {
+            uint256 public a;
+        }";
+        let res = parse_file(contents).validate(&OPTIONS);
+        assert_eq!(res.diags.len(), 1);
+        assert_eq!(res.diags[0].message, "missing NatSpec");
+    }
+
+    #[test]
+    fn test_variable_only_notice() {
+        let contents = "contract Test {
+            /// @notice The variable
+            uint256 public a;
+        }";
+        let res = parse_file(contents).validate(&OPTIONS);
+        assert_eq!(res.diags.len(), 1);
+        assert_eq!(res.diags[0].message, "@inheritdoc is missing");
+    }
+
+    #[test]
+    fn test_variable_multiline() {
+        let contents = "contract Test is ITest {
+            /**
+             * @inheritdoc ITest
+             */
+            uint256 public a;
+        }";
+        let res = parse_file(contents).validate(&OPTIONS);
+        assert!(res.diags.is_empty(), "{:#?}", res.diags);
+    }
+
+    #[test]
+    fn test_requires_inheritdoc() {
+        let contents = "contract Test is ITest {
+            uint256 public a;
+        }";
+        let res = parse_file(contents);
+        assert!(res.requires_inheritdoc());
+
+        let contents = "contract Test is ITest {
+            uint256 internal a;
+        }";
+        let res = parse_file(contents);
+        assert!(!res.requires_inheritdoc());
+    }
+
+    #[test]
+    fn test_variable_no_inheritdoc() {
+        let contents = "contract Test {
+            uint256 internal a;
+        }";
+        let res = parse_file(contents).validate(
+            &ValidationOptions::builder()
+                .inheritdoc(false)
+                .constructor(false)
+                .struct_params(false)
+                .enum_params(false)
+                .build(),
+        );
+        assert!(res.diags.is_empty(), "{:#?}", res.diags);
     }
 }
