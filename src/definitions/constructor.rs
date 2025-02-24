597f8a91aa8b980604c8af19105a1b0c2c485597
beeb <703631+beeb@users.noreply.github.com>
2025-02-24 09:32:07 +0100

    feat(constructor): allow to enforce natspec

--- src/definitions/constructor.rs
+++ src/definitions/constructor.rs
@@ -71,17 +71,21 @@ impl Validate for ConstructorDefinition {
             span: self.span(),
             diags: vec![],
         };
-        if !options.constructor || self.params.is_empty() {
-            return out;
-        }
-
         let Some(natspec) = &self.natspec else {
+            if (!options.constructor || self.params.is_empty())
+                && !options.enforce.contains(&ItemType::Constructor)
+            {
+                return out;
+            }
             out.diags.push(Diagnostic {
                 span: self.span(),
                 message: "missing NatSpec".to_string(),
             });
             return out;
         };
+        if !options.constructor || self.params.is_empty() {
+            return out;
+        }
         // check params
         out.diags.append(&mut check_params(natspec, &self.params));
         out
@@ -208,5 +212,19 @@ mod tests {
             parse_file(contents).validate(&ValidationOptions::builder().constructor(true).build());
         assert_eq!(res.diags.len(), 1);
         assert_eq!(res.diags[0].message, "@param param1 is missing");
+    }
+
+    #[test]
+    fn test_constructor_enforce() {
+        let contents = "contract Test {
+            constructor() { } 
+        }";
+        let res = parse_file(contents).validate(
+            &ValidationOptions::builder()
+                .enforce(vec![ItemType::Constructor])
+                .build(),
+        );
+        assert_eq!(res.diags.len(), 1);
+        assert_eq!(res.diags[0].message, "missing NatSpec");
     }
 }
