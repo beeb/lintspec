c51a7e25f1407b21e09873c15dfc803204da1f47
beeb <703631+beeb@users.noreply.github.com>
2025-05-13 17:13:40 +0200

    chore(bench): bench for longer

--- benches/solar.rs
+++ benches/solar.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use divan::{black_box, Bencher};
 use lintspec::{
     lint::{lint, ValidationOptions},
@@ -15,14 +17,14 @@ fn main() {
     divan::main();
 }
 
-#[divan::bench(args = FILES)]
+#[divan::bench(args = FILES, min_time = Duration::from_secs(1))]
 fn parse_slang(bencher: Bencher, path: &str) {
     let options = ValidationOptions::default();
     let parser = SlangParser::builder().skip_version_detection(true).build();
     bencher.bench_local(move || black_box(lint(parser.clone(), path, &options, false).unwrap()))
 }
 
-#[divan::bench(args = FILES)]
+#[divan::bench(args = FILES, min_time = Duration::from_secs(1))]
 fn parse_solar(bencher: Bencher, path: &str) {
     let options = ValidationOptions::default();
     let parser = SolarParser {};
