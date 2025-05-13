de0da5fc9671634d753a73082bffcb9aa3af73c5
beeb <703631+beeb@users.noreply.github.com>
2025-05-13 16:50:18 +0200

    chore: add benchmark

---
+++ benches/solar.rs
@@ -0,0 +1,30 @@
+use divan::{black_box, Bencher};
+use lintspec::{
+    lint::{lint, ValidationOptions},
+    parser::{slang::SlangParser, solar::SolarParser},
+};
+
+const FILES: &[&str] = &[
+    "test-data/BasicSample.sol",
+    "test-data/ParserTest.sol",
+    "test-data/InterfaceSample.sol",
+    "test-data/LibrarySample.sol",
+];
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench(args = FILES)]
+fn parse_slang(bencher: Bencher, path: &str) {
+    let options = ValidationOptions::default();
+    let parser = SlangParser::builder().skip_version_detection(true).build();
+    bencher.bench_local(move || black_box(lint(parser.clone(), path, &options, false).unwrap()))
+}
+
+#[divan::bench(args = FILES)]
+fn parse_solar(bencher: Bencher, path: &str) {
+    let options = ValidationOptions::default();
+    let parser = SolarParser {};
+    bencher.bench_local(move || black_box(lint(parser.clone(), path, &options, false).unwrap()))
+}
