87492a2efc33dea54f48078efd4d7257649229f8
beeb <703631+beeb@users.noreply.github.com>
2025-05-14 08:09:53 +0200

    chore(bench): only benchmark parsing part

--- benches/solar.rs
+++ benches/solar.rs
@@ -1,10 +1,7 @@
-use std::time::Duration;
+use std::{fs::File, time::Duration};
 
 use divan::{black_box, Bencher};
-use lintspec::{
-    lint::{lint, ValidationOptions},
-    parser::{slang::SlangParser, solar::SolarParser},
-};
+use lintspec::parser::{slang::SlangParser, solar::SolarParser, Parse, ParsedDocument};
 
 const FILES: &[&str] = &[
     "test-data/BasicSample.sol",
@@ -17,16 +14,21 @@ fn main() {
     divan::main();
 }
 
+fn parse_file(mut parser: impl Parse, path: &str) -> ParsedDocument {
+    let file = File::open(path).unwrap();
+    parser.parse_document(file, Some(path), false).unwrap()
+}
+
 #[divan::bench(args = FILES, min_time = Duration::from_secs(1))]
 fn parse_slang(bencher: Bencher, path: &str) {
-    let options = ValidationOptions::default();
     let parser = SlangParser::builder().skip_version_detection(true).build();
-    bencher.bench_local(move || black_box(lint(parser.clone(), path, &options, false).unwrap()))
+    bencher.bench_local(move || {
+        black_box(parse_file(parser.clone(), path));
+    })
 }
 
 #[divan::bench(args = FILES, min_time = Duration::from_secs(1))]
 fn parse_solar(bencher: Bencher, path: &str) {
-    let options = ValidationOptions::default();
     let parser = SolarParser {};
-    bencher.bench_local(move || black_box(lint(parser.clone(), path, &options, false).unwrap()))
+    bencher.bench_local(move || black_box(parse_file(parser.clone(), path)))
 }
