314c0e7227f0359e478a117b62a48eddfb005eac
beeb <703631+beeb@users.noreply.github.com>
2025-05-14 20:51:35 +0200

    chore(flake): remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,48 +1,59 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ fenix.overlays.default ];
-        };
-        lib = pkgs.lib;
-        toolchain = fenix.packages.${system}.fromToolchainFile { dir = ./.; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rust-analyzer-unwrapped
-            toolchain
-            pkgs.cargo-insta
-            pkgs.cargo-nextest
-            pkgs.cargo-dist
-          ];
-
-          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-        };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "lintspec";
-          inherit ((lib.importTOML ./Cargo.toml).package) version;
-
-          src = lib.cleanSource ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            allowBuiltinFetchGit = true;
+  outputs = { self, nixpkgs, fenix }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
           };
+          toolchain = fenix.packages.${system}.stable.toolchain;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo-dist
+              cargo-insta
+              cargo-nextest
+              rust-analyzer-unwrapped
+              toolchain
+            ];
 
-          doCheck = false;
-        };
-      });
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          lib = pkgs.lib;
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "lintspec";
+            inherit ((lib.importTOML ./Cargo.toml).package) version;
+
+            src = lib.cleanSource ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
+
+            doCheck = false;
+          };
+        }
+      );
+    };
 }
