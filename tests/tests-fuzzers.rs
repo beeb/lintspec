4519ba0920df5c0cb52f54e0b4559ea0f509fee6
Valentin B. <703631+beeb@users.noreply.github.com>
2025-05-15 09:44:17 +0200

    fix(slang): span start for variable definition with non-native type (#85)

---
+++ tests/tests-fuzzers.rs
@@ -0,0 +1,13 @@
+use lintspec::lint::ValidationOptions;
+
+mod common;
+use common::*;
+
+#[test]
+fn test_fuzzers() {
+    insta::assert_snapshot!(snapshot_content(
+        "./test-data/Fuzzers.sol",
+        &ValidationOptions::builder().inheritdoc(false).build(),
+        true,
+    ));
+}
