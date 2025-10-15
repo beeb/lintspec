07b30e6f1607cf630b2c531ae233b6d67f034d31
beeb <703631+beeb@users.noreply.github.com>
2025-10-15 16:27:52 +0200

    test: remove miette decorations and color during test

--- crates/lintspec/tests/tests-unicode-sample.rs
+++ crates/lintspec/tests/tests-unicode-sample.rs
@@ -1,11 +1,19 @@
 #![cfg(feature = "solar")]
 use lintspec::lint::ValidationOptions;
+use miette::GraphicalTheme;
 
 mod common;
 use common::*;
 
 #[test]
 fn test_unicode() {
+    let _ = miette::set_hook(Box::new(|_| {
+        Box::new(
+            miette::MietteHandlerOpts::new()
+                .graphical_theme(GraphicalTheme::none())
+                .build(),
+        )
+    }));
     insta::assert_snapshot!(snapshot_content(
         "./test-data/UnicodeSample.sol",
         &ValidationOptions::default(),
